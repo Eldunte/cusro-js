132376c6239079ee9187f406e472faa8693ae0eb
unknown <reinaldo_ramos@saojose.sc.senai.br>
2019-06-17 12:24:01 -0300

    Finalizando a aula 2 do modulo 3 apagando os consoles logs

--- mod3/aula/2/todos.js
+++ mod3/aula/2/todos.js
@@ -4,14 +4,9 @@ var inputElement = document.querySelector("#app input");
 
 var inputButton = document.querySelector("#app button");
 
-console.log(listElement);
-console.log(inputElement);
-console.log(inputButton)
-
 var itensTODO = [
-    `Fazer cafe`,
-    `Estudar pro ENEM`,
-    `entrar na comunidade`
-]
+    `*Fazer cafe`,
+    `*Estudar pro ENEM`,
+    `*entrar na comunidade`
+];
 
-console.log(itensTODO);
