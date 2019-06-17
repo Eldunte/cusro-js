e3c20efbf9cb8d785583e90d08b975b6835b6dcd
unknown <reinaldo_ramos@saojose.sc.senai.br>
2019-06-17 12:13:40 -0300

    Usando um queryselector para selecionar os elementos da index

--- mod3/aula/2/todos.js
+++ mod3/aula/2/todos.js
@@ -1,1 +1,9 @@
-alert("itens todo")
+var listElement = document.querySelector("#app ul");
+
+var inputElement = document.querySelector("#app input");
+
+var inputButton = document.querySelector("#app button");
+
+console.log(listElement);
+console.log(inputElement);
+console.log(inputButton)
