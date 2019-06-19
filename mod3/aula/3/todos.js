8c50bc1961e9d39a4084ab8f6824253fe8e82d0d
unknown <reinaldo_ramos@saojose.sc.senai.br>
2019-06-19 12:18:09 -0300

    comecando a aula 3, mudei o título

---
+++ mod3/aula/3/todos.js
@@ -0,0 +1,12 @@
+var listElement = document.querySelector("#app ul");
+
+var inputElement = document.querySelector("#app input");
+
+var inputButton = document.querySelector("#app button");
+
+var itensTODO = [
+    `*Fazer cafe`,
+    `*Estudar pro ENEM`,
+    `*entrar na comunidade`
+];
+
