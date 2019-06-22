1e3da07ab317c967602296b12f94c9541f5f5df9
Eldunte <reinaldo_ramos@estudante.sc.senai.br>
2019-06-22 16:18:18 -0300

    comecando a aula 4 e criando a função addtodo

---
+++ mod3/aula/4/todos.js
@@ -0,0 +1,24 @@
+var listElement = document.querySelector("#app ul");
+
+var inputElement = document.querySelector("#app input");
+
+var inputButton = document.querySelector("#app button");
+
+var itensTODO = [
+    `Fazer cafe`,
+    `Estudar pro ENEM`,
+    `entrar na comunidade`
+];
+
+function renderizarTODOS()
+{
+    for(todo of itensTODO){
+        var todoElement = document.createElement('li')
+        var todoText = document.createTextNode(todo)
+
+        todoElement.appendChild(todoText);
+        listElement.appendChild(todoElement);
+    };
+};
+
+renderizarTODOS();
